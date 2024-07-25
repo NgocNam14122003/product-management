bc163a8ac3e3dbb75776dc11fe825b6c9d20f25c
NgocNam14122003 <lelinh5ethdt@gmail.com>
2024-07-25 23:06:47 +0700

    Xây dựng phần chỉnh sửa nhóm quyền

--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const controller = require("../../controllers/admin/role.controller");
 
 router.get("/", controller.index);
-
-router.get("/create", controller.create)
-router.post("/create", controller.createPost)
+router.get("/create", controller.create);
+router.post("/create", controller.createPost);
+router.get("/edit/:id", controller.edit);
+router.patch("/edit/:id", controller.editPatch);
 
 module.exports = router;
